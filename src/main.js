40cd3b59450304e64f81e27b4175d88544f3994d
Yuriy Andreev <andreyev.yury@gmail.com>
2024-08-28 19:56:43 +0300

    m1t3 feat: view filters

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 
+import Filters from './view/Filters.js';
+import { render, RenderPosition } from './render.js';
+
+const tripControlsFilters = document.querySelector('.trip-controls__filters');
+
+
+render(new Filters(), tripControlsFilters, RenderPosition.BEFOREEND);
