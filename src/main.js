8e5575a3b15f8d87351d14e051522330682732c8
Yuriy Andreev <andreyev.yury@gmail.com>
2024-08-28 20:02:40 +0300

    m1t3 feat: view sort

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 
 import Filters from './view/Filters.js';
+import Sort from './view/Sort.js';
 import { render, RenderPosition } from './render.js';
 
 const tripControlsFilters = document.querySelector('.trip-controls__filters');
+const tripEvents = document.querySelector('.trip-events');
 
 
 render(new Filters(), tripControlsFilters, RenderPosition.BEFOREEND);
+render(new Sort(), tripEvents, RenderPosition.BEFOREEND);
